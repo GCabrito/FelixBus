927c4de9d8490d8a9ed73d6befd11cd37c0c9d24
Gonçalo <gcabrito2005@gmail.com>
2024-12-30 02:35:57 +0000

    style.css, register.html, register.php

--- register.php
+++ register.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    session_start();
+
+    //ligar à base de dados
+    $host = 'localhost';
+    $dbusername = 'root';
+    $dbpassword = '';
+    $dbname = 'FelixBus';
+
+    $username = $_POST["username"];
+    $completeName = $_POST["completeName"];
+    $address = $_POST["address"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirmPassword = $_POST["confirm-password"];
+
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+    if ($password != $confirmPassword) {
+        echo '<script> alert("Palavras-passe não coincidem");
+              window.location.href = "register.html"</script>';
+    }
+?>
