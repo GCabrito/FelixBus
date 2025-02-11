1c1b18ac279db478430b0e33ef457e0d3712b683
Gonçalo <gcabrito2005@gmail.com>
2025-02-11 19:08:41 +0000

    Versão final

--- paginas/loginVerification.php
+++ paginas/loginVerification.php
@@ -2,6 +2,7 @@
     session_start();
 
     if (!isset($_SESSION['email'])) {
+        echo ('<script>alert("É necessário efetuar login");</script>');
         echo ('<script>window.location.href = "login.html";</script>');
         exit;
     }
